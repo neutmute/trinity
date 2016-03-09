fd1d5272f8877c1fd2de9585419424b870cf2855
neutmute <neutmute@users.noreply.github.com>
2016-03-10 10:54:54 +1100

    setup for team city assembly info stamping

--- src/Trinity/Properties/AssemblyInfo.cs
+++ src/Trinity/Properties/AssemblyInfo.cs
@@ -1,12 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Trinity")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trinity")]
+#if DEBUG
+[assembly: AssemblyProduct("Trinity (Debug)")]
+[assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyProduct("Trinity (Release)")]
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("Developer Build")]
